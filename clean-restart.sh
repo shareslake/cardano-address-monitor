8e146058be05beec101af67ba96343d2d3e685e6
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2022-07-07 11:52:51 +0000

    Add db-sync submodule
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- clean-restart.sh
+++ clean-restart.sh
@@ -14,10 +14,10 @@ docker-compose up postgresql -d
 
 sleep 5
 
-PGPASSFILE=./pgpass-mainnet ./postgresql-setup.sh --createdb
+PGPASSFILE=./pgpass-mainnet ./shareslake-db-sync/scripts/postgresql-setup.sh --createdb
 
 #TODO: use nohup to maintain the process running after exiting
-PGPASSFILE=./pgpass-mainnet cardano-db-sync     --config ./mainnet-config.yaml     --socket-path /opt/shareslake/node-ipc/node.sock     --state-dir ./ledger-state/mainnet     --schema-dir ./schema/ > ./db-sync.log 2>&1 &
+PGPASSFILE=./pgpass-mainnet cardano-db-sync --config ./mainnet-config.yaml --socket-path /opt/shareslake/node-ipc/node.sock --state-dir ./ledger-state/mainnet --schema-dir ./shareslake-db-sync/schema/ > ./db-sync.log 2>&1 &
 
 sleep 3
 PGPASSFILE=./pgpass-mainnet psql -Upostgres -h localhost -p 5432 --dbname cexplorer -a -f schema.sql
--- restart.sh
+++ restart.sh
@@ -3,8 +3,6 @@ set -o nounset
 set -o pipefail
 # set -o xtrace # Uncomment this line for debugging purpose
 
-set -m
-
 export LD_LIBRARY_PATH="/opt/shareslake/lib"
 
 docker-compose up postgresql -d
@@ -12,7 +10,5 @@ docker-compose up postgresql -d
 sleep 5
 
 #TODO: use nohup to maintain the process running after exiting
-PGPASSFILE=./pgpass-mainnet cardano-db-sync     --config ./mainnet-config.yaml     --socket-path /opt/shareslake/node-ipc/node.sock     --state-dir ./ledger-state/mainnet     --schema-dir ./schema/ > ./db-sync.log 2>&1 &
-
-fg
+PGPASSFILE=./pgpass-mainnet cardano-db-sync --config ./mainnet-config.yaml --socket-path /opt/shareslake/node-ipc/node.sock --state-dir ./ledger-state/mainnet --schema-dir ./shareslake-db-sync/schema/
 
